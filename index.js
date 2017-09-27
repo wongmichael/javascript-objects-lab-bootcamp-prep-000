3323ef0d0a6b56e05036f448dff2f5d2229b3b14
Michael Wong <mik9080@gmail.com>
2017-09-27 14:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function updateObjectWithKeyAndValue(object,key,value){
 function updateObjectWithKeyAndValue(object,key,value){
   return Object.assign({},object,{[key]:value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  return object[key]=value
+}
